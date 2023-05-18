cd9f3dc86e1f79f5d90b142a0de4cd68fd2e67d5
StoneFigure <caleb.leman@meritknights.com>
2023-05-18 09:10:09 -0600

    Finished

---
+++ src/config.ts
@@ -0,0 +1,291 @@
+//Temp Imported Pictures
+import Temp from "./assets/images/Temp.png";
+import Temp2 from "./assets/images/Temp2.jpg";
+import Temp3 from "./assets/images/Temp3.jpg";
+
+//Copy and Paste Template
+export const EXAMPLE = {
+    TEXT: {
+
+    },
+    LINKS: {
+        EX: {
+            href: "#",
+            content: "Ex",
+        },
+    },
+    IMAGES: {
+        P1: {
+            src: Temp,
+            alt: "Ex",
+        },
+    },
+}
+
+
+
+//Imported on Navbar
+export const NAVBAR = {
+    TEXT: {
+
+    },
+    LINKS: {
+        B1: {
+            href: "/#",
+            content: "Home",
+        },
+        B2: {
+            href: "/pricing",
+            content: "Pricing",
+        },
+        B3: {
+            href: "/AboutUs",
+            content: "About Us",
+        },
+        B4: {
+            href: "/ContactPage",
+            content: "Contact Us",
+        }
+    },
+}
+
+//Imported on Hero
+export const HERO = {
+    TEXT: {
+        H1: "Welcome to",
+        H2: "Brand Name",
+
+    },
+    LINKS: {
+
+    },
+    IMAGES: {
+        MAIN: {
+            src: Temp,
+            alt: "Temporary",
+        },
+    },
+}
+
+//Imported on Divider
+export const DIVIDER = {
+    TEXT: {
+        H1: "Our Things",
+        BH1: "Learn about our product",
+        CH1: "Lorem ipsum dolor sit amet",
+        CH2: "Lorem ipsum dolor sit amet",
+        CH3: "Lorem ipsum dolor sit amet",
+        CH4: "Lorem ipsum dolor sit amet",
+
+    },
+    LINKS: {
+
+    },
+    IMAGES: {
+        P1: {
+            src: Temp2,
+            alt: "Temporary",
+        },
+        P2: {
+            src: Temp3,
+            alt: "Temporary",
+        },
+    },
+}
+
+//Imported on Hero Pricing
+export const HEROPRICE = {
+    TEXT: {
+        H1: "Affordable",
+        H2: "Pricing"
+    },
+    LINKS: {
+        
+    },
+    IMAGES: {
+        MAIN: {
+            src: Temp,
+            alt: "Temp",
+        },
+    },
+}
+
+//Imported on Divider Pricing
+export const DIVIDERPRICE = {
+    TEXT: {
+        H1: "Learn about our product",
+        CH1: "Lorem ipsum dolor sit amet",
+        CH2: "Lorem ipsum dolor sit amet",
+        CH3: "Lorem ipsum dolor sit amet",
+        CH4: "Lorem ipsum dolor sit amet",
+        B1H: "Product",
+        B2H: "Product2",
+        B3H: "Product3",
+        B1P: "Our Price",
+        B2P: "Our Price",
+        B3P: "Our Price",
+
+    },
+    LINKS: {
+        B1: {
+            href: "/ItemOnePage",
+            content: "Product 1",
+        },
+        B2: {
+            href: "/ItemTwoPage",
+            content: "Product 2",
+        },
+        B3: {
+            href: "/ItemThreePage",
+            content: "Product 3",
+        },
+
+    },
+    IMAGES: {
+        P1: {
+            src: Temp2,
+            alt: "Temporary",
+        },
+        P2: {
+            src: Temp3,
+            alt: "Temporary",
+        },
+        P3: {
+            src: Temp3,
+            alt: "Temporary",
+        },
+        P4: {
+            src: Temp3,
+            alt: "Temporary",
+        },
+    },
+}
+
+//imported on ItemONe
+
+export const ITEMONE = {
+    TEXT: {
+
+        B1H: "Product",
+        B2H: "Our Item Desc",
+
+        B1P: "Our Price",
+        B2P: "Our Price",
+
+
+    },
+    LINKS: {
+
+    },
+    IMAGES: {
+        P1: {
+            src: Temp2,
+            alt: "Temporary",
+        },
+
+    },
+}
+//Imported on Item Two
+export const ITEMTWO = {
+    TEXT: {
+
+        B1H: "Product",
+        B2H: "Our Item Desc",
+        B1P: "Our Price",
+        B2P: "Our Price",
+
+
+    },
+    LINKS: {
+
+    },
+    IMAGES: {
+        P1: {
+            src: Temp2,
+            alt: "Temporary",
+        },
+
+    },
+}
+
+//Imported on ItemThree
+export const ITEMTHREE = {
+    TEXT: {
+
+        B1H: "Product",
+        B2H: "Our Item Desc",
+        B1P: "Our Price",
+        B2P: "Our Price",
+
+
+    },
+    LINKS: {
+
+    },
+    IMAGES: {
+        P1: {
+            src: Temp2,
+            alt: "Temporary",
+        },
+
+    },
+}
+
+//Imported on About
+
+export const ABOUT = {
+    TEXT: {
+        H1: "About Us",
+        P1: "Person 1",
+        P2: "Person 2",
+        P3: "Person 3",
+        P1C: "Info",
+        P2C: "Info",
+        P3C: "Info",
+
+    },
+    LINKS: {
+        L1: {
+            href: "https://www.youtube.com",
+            content: "Youtube",
+        },
+        L2: {
+            href: "https://www.twitter.com",
+            content: "Twitter",
+        },
+        L3: {
+            href: "https://www.facebook.com",
+            content: "Facebook",
+        },
+        L4: {
+            href: "https://www.instagram.com",
+            content: "Instagram",
+        }
+    },
+    IMAGES: {
+        P1: {
+            src: Temp,
+            alt: "Temporary",
+        },
+        P2: {
+            src: Temp,
+            alt: "Temporary",
+        },
+        P3: {
+            src: Temp,
+            alt: "Temporary",
+        },
+    },
+}
+
+export const CONTACT = {
+    TEXT: {
+        
+    },
+    LINKS: {
+
+    },
+    IMAGES: {
+
+    },
+}
